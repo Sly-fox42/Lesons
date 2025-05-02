fd89b8f8052475f95f9d1c9ea86e6947f630555f
Igor Bazanov <site-create-x@yandex.ru>
2025-05-02 13:55:36 +0700

    Решены новые задаччи 68, 70

---
+++ DZ_70/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DZ_70
+{
+    internal class Program
+    {
+
+        static int[] ResizeArr(ref int[] array)
+        { 
+            int newSize;
+            Console.Write("Введите новый размер массива: ");
+           newSize =int.Parse(Console.ReadLine());  
+            int [] newArray = new int[newSize];
+
+            for (int i = 0; i <array.Length && i< newSize; i++) {
+                newArray[i] = array[i];
+            }
+              
+           return array = newArray;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 3,58,899,8,
+            40};
+
+            ResizeArr(ref arr);
+            Console.WriteLine(arr.Length);
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write(arr[i] +" ");
+            }
+
+            Console.WriteLine(arr.Length);
+        }
+    }
+}
